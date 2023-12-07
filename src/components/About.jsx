de7b2407f8d82f8185ea81de8c2d6f662a0e8569
ansaralyh <ansaralyh@gmail.com>
2023-12-08 00:19:26 +0500

    About section done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import './styles.css'
 
 const About = () => {
 
 
   return (
-    <>
-    <h1>About us</h1>
-    </>
+    <div className='container about'>
+    <h1 className='about-h'>Our journey</h1>
+    <p className='about-para1'>Welcome to Hb & SONS LLC, where our commitment to excellence fuels our journey. Founded with a vision to redefine fuel industry standards, our story is one of dedication and continuous growth.</p>
+    <p className='about-p2'>Explore the journey that shapes who we are – a trusted partner in fuel excellence.</p>
+    <button className='about-btn'>Get in Touch <img src="/Images/Vector.png" alt="" /></button>
+    </div>
   )
 }
 
